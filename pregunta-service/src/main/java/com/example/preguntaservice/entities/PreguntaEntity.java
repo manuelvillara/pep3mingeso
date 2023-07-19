3006d351856a4fe51509fdecf72a795452b19610
Manuel Villar <manuel.villar@usach.cl>
2023-07-19 18:26:49 -0400

    Cambios para probar gatewar
    Ya funcionan los 3, el pregunta, el config y el eureka y además se colocan en los puertos que manda el config. ahora hice los cambios en el archivo del gateaway de la carpeta config-data para ver si funciona todo

--- pregunta-service/src/main/java/com/example/preguntaservice/entities/PreguntaEntity.java
+++ pregunta-service/src/main/java/com/example/preguntaservice/entities/PreguntaEntity.java
@@ -1,10 +1,12 @@
 package com.example.preguntaservice.entities;
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "pregunta")
